a308a7e2a7595171506a7a58582dea16856b300b
brnewby602 <brnewby602@gmail.com>
2016-04-02 16:12:27 -0700

    Sort links by visit count

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -4,9 +4,14 @@ angular.module('shortly.links', [])
   // Your code here
   $scope.data = {};
 
+  $scope.filter = '';
+
   console.log('inside LinksController');
   Links.getAll()
     .then(function(links) {
+      links = links.sort(function(a, b) {
+        return b.visits - a.visits;
+      });
       $scope.data.links = links;
       console.log('links = ' + JSON.stringify(links));
     })
