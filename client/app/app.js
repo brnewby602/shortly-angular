3062784946360fd0719e61bdd38e5afdbc5db083
brnewby602 <brnewby602@gmail.com>
2016-04-02 11:34:10 -0700

    Authenticate user for links and shorten reqs

--- client/app/app.js
+++ client/app/app.js
@@ -17,11 +17,13 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     });
     // Your code here
 
@@ -47,16 +49,36 @@ angular.module('shortly', [
   return attach;
 })
 .run(function ($rootScope, $location, Auth) {
-  // here inside the run phase of angular, our services and controllers
-  // have just been registered and our app is ready
-  // however, we want to make sure the user is authorized
-  // we listen for when angular is trying to change routes
-  // when it does change routes, we then look for the token in localstorage
-  // and send that token to the server to see if it is a real user or hasn't expired
-  // if it's not valid, we then redirect back to signin/signup
+  // here inside the run phase of angular, our services and controllers have
+  // just been registered and our app is ready however, we want to make sure
+  // the user is authorized we listen for when angular is trying to change
+  // routes when it does change routes, we then look for the token in
+  // localstorage and send that token to the server to see if it is a real
+  // user or hasn't expired if it's not valid, we then redirect back to
+  // signin/signup
+
+  console.log('******************IM IN HERE**************');
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/signin');
     }
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
